5625a385f3a06e6402ca63f3789f9cff6d302fd4
mbringas <56489741+mbringas@users.noreply.github.com>
2023-10-06 10:14:20 -0300

    Update 2_create_lag_variables.py

--- segunda_competencia_paso_a_paso/scripts_for_cloud/2_create_lag_variables.py
+++ segunda_competencia_paso_a_paso/scripts_for_cloud/2_create_lag_variables.py
@@ -6,12 +6,39 @@ con = duckdb.connect()
 # Load data from CSV file into a DuckDB table
 con.execute("CREATE TABLE bank_data AS SELECT * FROM read_csv_auto('~/buckets/b1/datasets/competencia_02_quantile_features.csv.gz');")
 
-columns=["active_quarter","cliente_vip","internet","cproductos","tcuentas","ccuenta_corriente","mcuenta_corriente_adicional","mcuenta_corriente","ccaja_ahorro","mcaja_ahorro","mcaja_ahorro_adicional","mcaja_ahorro_dolares","cdescubierto_preacordado","mcuentas_saldo","ctarjeta_debito","ctarjeta_debito_transacciones","ctarjeta_visa","ctarjeta_visa_transacciones","ctarjeta_master","ctarjeta_master_transacciones","cprestamos_personales","mprestamos_personales","cprestamos_prendarios","mprestamos_prendarios","cprestamos_hipotecarios","mprestamos_hipotecarios","cplazo_fijo","mplazo_fijo_dolares","mplazo_fijo_pesos","cinversion1","minversion1_pesos","minversion1_dolares","cinversion2","minversion2","cseguro_vida","cseguro_auto","cseguro_vivienda","cseguro_accidentes_personales","ccaja_seguridad","cpayroll_trx","mpayroll2","cpayroll2_trx","ccuenta_debitos_automaticos","ctarjeta_visa_debitos_automaticos","mttarjeta_visa_debitos_automaticos","ctarjeta_master_debitos_automaticos","mttarjeta_master_debitos_automaticos","cpagodeservicios","mpagodeservicios","cpagomiscuentas","mpagomiscuentas","ccajeros_propios_descuentos","mcajeros_propios_descuentos","ctarjeta_visa_descuentos","mtarjeta_visa_descuentos","ctarjeta_master_descuentos","mtarjeta_master_descuentos","ccomisiones_mantenimiento","ccomisiones_otras","cforex","cforex_buy","mforex_buy","cforex_sell","mforex_sell","ctransferencias_recibidas","mtransferencias_recibidas","ctransferencias_emitidas","mtransferencias_emitidas","cextraccion_autoservicio","mextraccion_autoservicio","ccheques_depositados","mcheques_depositados","ccheques_emitidos","mcheques_emitidos","ccheques_depositados_rechazados","mcheques_depositados_rechazados","ccheques_emitidos_rechazados","mcheques_emitidos_rechazados","tcallcenter","ccallcenter_transacciones","thomebanking","chomebanking_transacciones","ccajas_transacciones","ccajas_consultas","ccajas_depositos","ccajas_extracciones","ccajas_otras","catm_trx","matm","catm_trx_other","matm_other","ctrx_quarter","tmobile_app","cmobile_app_trx","Master_delinquency","Master_status","Master_mfinanciacion_limite","Master_Fvencimiento","Master_Finiciomora","Master_msaldototal","Master_msaldopesos","Master_msaldodolares","Master_mconsumospesos","Master_mconsumosdolares","Master_mlimitecompra","Master_madelantopesos","Master_madelantodolares","Master_fultimo_cierre","Master_mpagado","Master_mpagospesos","Master_mpagosdolares","Master_fechaalta","Master_mconsumototal","Master_cconsumos","Master_cadelantosefectivo","Master_mpagominimo","Visa_delinquency","Visa_status","Visa_mfinanciacion_limite","Visa_Fvencimiento","Visa_Finiciomora","Visa_msaldototal","Visa_msaldopesos","Visa_msaldodolares","Visa_mconsumospesos","Visa_mconsumosdolares","Visa_mlimitecompra","Visa_madelantopesos","Visa_madelantodolares","Visa_fultimo_cierre","Visa_mpagado","Visa_mpagospesos","Visa_mpagosdolares","Visa_fechaalta","Visa_mconsumototal","Visa_cconsumos","Visa_cadelantosefectivo","Visa_mpagominimo"]
+intact=["active_quarter","cliente_vip","internet","cproductos","tcuentas","ccuenta_corriente","ccaja_ahorro","mcaja_ahorro_adicional",
+	"mcaja_ahorro_dolares","cdescubierto_preacordado","ctarjeta_debito","ctarjeta_debito_transacciones","ctarjeta_visa",
+	"ctarjeta_visa_transacciones","ctarjeta_master","ctarjeta_master_transacciones","cprestamos_personales",
+	"cprestamos_prendarios","cprestamos_hipotecarios","cplazo_fijo","cinversion1","cinversion2","minversion2",
+	"cseguro_vida","cseguro_auto","cseguro_vivienda","cseguro_accidentes_personales","ccaja_seguridad","cpayroll_trx",
+	"mpayroll2","cpayroll2_trx","ccuenta_debitos_automaticos","ctarjeta_visa_debitos_automaticos","ctarjeta_master_debitos_automaticos",
+	"cpagodeservicios","cpagomiscuentas","mpagomiscuentas","ccajeros_propios_descuentos","mcajeros_propios_descuentos","ctarjeta_visa_descuentos",
+	"mtarjeta_visa_descuentos","ctarjeta_master_descuentos","mtarjeta_master_descuentos","ccomisiones_mantenimiento",
+	"ccomisiones_otras","cforex","cforex_buy","mforex_buy","cforex_sell","mforex_sell","ctransferencias_recibidas",
+	"ctransferencias_emitidas","cextraccion_autoservicio","ccheques_depositados","ccheques_emitidos","ccheques_depositados_rechazados",
+	"ccheques_emitidos_rechazados","tcallcenter","ccallcenter_transacciones","thomebanking","chomebanking_transacciones","ccajas_transacciones",
+	"ccajas_consultas","ccajas_depositos","ccajas_extracciones","ccajas_otras","catm_trx","catm_trx_other","matm_other","ctrx_quarter",
+	"tmobile_app","cmobile_app_trx","Master_delinquency","Master_status","Master_mfinanciacion_limite","Master_Fvencimiento","Master_Finiciomora",
+	"Master_msaldopesos","Master_msaldodolares","Master_mconsumospesos","Master_mconsumosdolares","Master_mlimitecompra",
+	"Master_madelantopesos","Master_madelantodolares","Master_fultimo_cierre","Master_mpagospesos","Master_mpagosdolares",
+	"Master_fechaalta","Master_mconsumototal","Master_cconsumos","Master_cadelantosefectivo","Master_mpagominimo","Visa_delinquency",
+	"Visa_status","Visa_mfinanciacion_limite","Visa_Fvencimiento","Visa_Finiciomora","Visa_msaldopesos","Visa_msaldodolares",
+	"Visa_mconsumospesos","Visa_mconsumosdolares","Visa_mlimitecompra","Visa_madelantopesos","Visa_madelantodolares","Visa_fultimo_cierre",
+	"Visa_mpagospesos","Visa_mpagosdolares","Visa_fechaalta","Visa_mconsumototal","Visa_cconsumos","Visa_cadelantosefectivo",
+	"Visa_mpagominimo"]
 
-columns=columns+["mrentabilidad","mrentabilidad_annual","mcomisiones","mactivos_margen","mpasivos_margen","mautoservicio","mtarjeta_visa_consumo","mtarjeta_master_consumo","mpayroll","mcuenta_debitos_automaticos","mcomisiones_mantenimiento","mcomisiones_otras"]
+columns=["mcuenta_corriente_adicional","mcuenta_corriente","mcaja_ahorro","mcuentas_saldo","mprestamos_personales","mprestamos_prendarios",
+	 "mprestamos_hipotecarios","mplazo_fijo_dolares","mplazo_fijo_pesos","minversion1_pesos","minversion1_dolares",
+	 "mttarjeta_visa_debitos_automaticos","mttarjeta_master_debitos_automaticos","mpagodeservicios","mtransferencias_recibidas",
+	 "mtransferencias_emitidas","mextraccion_autoservicio","mcheques_depositados","mcheques_emitidos","mcheques_depositados_rechazados",
+	 "mcheques_emitidos_rechazados","matm","Visa_mpagado","Master_mpagado","Master_msaldototal","Visa_msaldototal"]
+	
+columns=columns+["q_mrentabilidad","q_mrentabilidad_annual","q_mcomisiones","q_mactivos_margen","q_mpasivos_margen","q_mautoservicio",
+		 "q_mtarjeta_visa_consumo","q_mtarjeta_master_consumo","q_mpayroll","q_mcuenta_debitos_automaticos",
+		 "q_mcomisiones_mantenimiento","q_mcomisiones_otras"]
 
 
-nuevos_features=""
+nuevos_features=",".join(intact)
 for column in columns:
     nuevos_features += f"""\n, LAG({column}) OVER (PARTITION BY numero_de_cliente ORDER BY foto_mes) AS prev_{column}
 			   \n, {column} - LAG({column}) OVER (PARTITION BY numero_de_cliente ORDER BY foto_mes) AS difference_{column}  
