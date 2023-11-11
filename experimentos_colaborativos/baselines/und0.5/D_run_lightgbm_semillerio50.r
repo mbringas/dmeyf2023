bc5d4bddd4fa266525367b92099adc2290292029
mbringas <maubringas@gmail.com>
2023-11-11 23:19:26 +0000

    ll

--- experimentos_colaborativos/baselines/und0.5/D_run_lightgbm_semillerio50.r
+++ experimentos_colaborativos/baselines/und0.5/D_run_lightgbm_semillerio50.r
@@ -1,12 +1,7 @@
 # para correr el Google Cloud
 #   8 vCPU
-#  16 GB memoria RAM
+#  64 GB memoria RAM
 
-# LR,feature fraction,min_data_in_leaves,num_leaves,nombre, num_iterations
-
-best_model<-c(c(0.03,0.325,101,480,"BO3_model1",1922) )
-
-semilla <- 369821
 
 # limpio la memoria
 rm(list = ls()) # remove all objects
@@ -19,22 +14,56 @@ require("lightgbm")
 # defino los parametros de la corrida, en una lista, la variable global  PARAM
 #  muy pronto esto se leera desde un archivo formato .yaml
 PARAM <- list()
-PARAM$experimento <-"EXPCOL_mejormodelo_50semillas_0.5"
+PARAM$experimento <- "baseline_undersampling05_100seeds"
 
 PARAM$input$dataset <- "./datasets/colaborativos_features.csv.gz"
 
 # meses donde se entrena el modelo
-PARAM$input$training <- c(202012,202101, 202102, 202103, 202104, 202105)
-PARAM$input$future <- c(202106) # meses donde se aplica el modelo
+PARAM$input$training <- c(202012, 202101, 202102, 202103, 202104, 202105)
+PARAM$input$future <- c(202107) # meses donde se aplica el modelo
 
-PARAM$finalmodel$num_iterations <- best_model[6]#4928
-PARAM$finalmodel$learning_rate <- best_model[1]#0.0189943331895954
-PARAM$finalmodel$feature_fraction <- best_model[2]#0.892623977897483
-PARAM$finalmodel$min_data_in_leaf best_model[3]<- #785
-PARAM$finalmodel$num_leaves <- best_modeli[4] #666
+PARAM$finalmodel$semilla <- 102191
+
+# hiperparametros intencionalmente NO optimos
+PARAM$finalmodel$optim$num_iterations <- 1037
+PARAM$finalmodel$optim$learning_rate <- 0.06909
+PARAM$finalmodel$optim$feature_fraction <- 0.5427
+PARAM$finalmodel$optim$min_data_in_leaf <- 15534
+PARAM$finalmodel$optim$num_leaves <- 120
 
 
-PARAM$finalmodel$max_bin <- 31
+# Hiperparametros FIJOS de  lightgbm
+PARAM$finalmodel$lgb_basicos <- list(
+  boosting = "gbdt", # puede ir  dart  , ni pruebe random_forest
+  objective = "binary",
+  metric = "custom",
+  first_metric_only = TRUE,
+  boost_from_average = TRUE,
+  feature_pre_filter = FALSE,
+  force_row_wise = TRUE, # para reducir warnings
+  verbosity = -100,
+  max_depth = -1L, # -1 significa no limitar,  por ahora lo dejo fijo
+  min_gain_to_split = 0.0, # min_gain_to_split >= 0.0
+  min_sum_hessian_in_leaf = 0.001, #  min_sum_hessian_in_leaf >= 0.0
+  lambda_l1 = 0.0, # lambda_l1 >= 0.0
+  lambda_l2 = 0.0, # lambda_l2 >= 0.0
+  max_bin = 31L, # lo debo dejar fijo, no participa de la BO
+
+  bagging_fraction = 1.0, # 0.0 < bagging_fraction <= 1.0
+  pos_bagging_fraction = 1.0, # 0.0 < pos_bagging_fraction <= 1.0
+  neg_bagging_fraction = 1.0, # 0.0 < neg_bagging_fraction <= 1.0
+  is_unbalance = FALSE, #
+  scale_pos_weight = 1.0, # scale_pos_weight > 0.0
+
+  drop_rate = 0.1, # 0.0 < neg_bagging_fraction <= 1.0
+  max_drop = 50, # <=0 means no limit
+  skip_drop = 0.5, # 0.0 <= skip_drop <= 1.0
+
+  extra_trees = TRUE, # Magic Sauce
+
+  seed = PARAM$finalmodel$semilla
+)
+
 
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
@@ -43,6 +72,20 @@ setwd("~/buckets/b1")
 
 # cargo el dataset donde voy a entrenar
 dataset <- fread(PARAM$input$dataset, stringsAsFactors = TRUE)
+
+
+# Catastrophe Analysis  -------------------------------------------------------
+# deben ir cosas de este estilo
+#   dataset[foto_mes == 202006, active_quarter := NA]
+
+# Data Drifting
+# por ahora, no hago nada
+
+
+# Feature Engineering Historico  ----------------------------------------------
+#   aqui deben calcularse los  lags y  lag_delta
+#   Sin lags no hay paraiso ! corta la bocha
+#   https://rdrr.io/cran/data.table/man/shift.html
 
 
 #--------------------------------------
@@ -73,87 +116,75 @@ dir.create(paste0("./exp/", PARAM$experimento, "/"), showWarnings = FALSE)
 # Establezco el Working Directory DEL EXPERIMENTO
 setwd(paste0("./exp/", PARAM$experimento, "/"))
 
-for (i in 1:50){     
-     dir.create(paste0("./exp/", PARAM$experimento, "/", str(i) ,"/"), showWarnings = FALSE)
-     setwd(paste0("./exp/", PARAM$experimento, "/", str(i) ,"/"))
 
-     # dejo los datos en el formato que necesita LightGBM
-     dtrain <- lgb.Dataset(
-       data = data.matrix(dataset[train == 1L, campos_buenos, with = FALSE]),
-       label = dataset[train == 1L, clase01]
-     )
-     
-     # genero el modelo
-     # estos hiperparametros  salieron de una laaarga Optmizacion Bayesiana
-     modelo <- lgb.train(
-       data = dtrain,
-       param = list(
-         objective = "binary",
-         max_bin = PARAM$finalmodel$max_bin,
-         learning_rate = PARAM$finalmodel$learning_rate,
-         num_iterations = PARAM$finalmodel$num_iterations,
-         num_leaves = PARAM$finalmodel$num_leaves,
-         min_data_in_leaf = PARAM$finalmodel$min_data_in_leaf,
-         feature_fraction = PARAM$finalmodel$feature_fraction,
-         seed = semilla
-       )
-     )
-     
-     #--------------------------------------
-     # ahora imprimo la importancia de variables
-     tb_importancia <- as.data.table(lgb.importance(modelo))
-     archivo_importancia <- "impo.txt"
-     
-     fwrite(tb_importancia,
-       file = archivo_importancia,
-       sep = "\t"
-     )
-     
-     #--------------------------------------
-     
-     
-     # aplico el modelo a los datos sin clase
-     dapply <- dataset[foto_mes == PARAM$input$future]
-     
-     # aplico el modelo a los datos nuevos
-     prediccion <- predict(
-       modelo,
-       data.matrix(dapply[, campos_buenos, with = FALSE])
-     )
-     
-     # genero la tabla de entrega
-     tb_entrega <- dapply[, list(numero_de_cliente, foto_mes)]
-     tb_entrega[, prob := prediccion]
-     
-     # grabo las probabilidad del modelo
-     fwrite(tb_entrega,
-       file = "prediccion.txt",
-       sep = "\t"
-     )
-     
-     # ordeno por probabilidad descendente
-     setorder(tb_entrega, -prob)
-     
-     
-     # genero archivos con los  "envios" mejores
-     # deben subirse "inteligentemente" a Kaggle para no malgastar submits
-     # si la palabra inteligentemente no le significa nada aun
-     # suba TODOS los archivos a Kaggle
-     # espera a la siguiente clase sincronica en donde el tema sera explicado
-     
-     cortes <- seq(8000, 13000, by = 500)
-     for (envios in cortes) {
-       tb_entrega[, Predicted := 0L]
-       tb_entrega[1:envios, Predicted := 1L]
-     
-       fwrite(tb_entrega[, list(numero_de_cliente, Predicted)],
-         file = paste0(PARAM$experimento, "_", envios, ".csv"),
-         sep = ","
-       )
-     }
-     cat("\n\nLa generacion de los archivos para Kaggle ha terminado\n")
-     setwd(paste0("./exp/", PARAM$experimento, "/"))
+for (num in 1:100){
+# dejo los datos en el formato que necesita LightGBM
+dtrain <- lgb.Dataset(
+  data = data.matrix(dataset[train == 1L, campos_buenos, with = FALSE]),
+  label = dataset[train == 1L, clase01]
+)
 
+
+# genero el modelo
+param_completo <- c(PARAM$finalmodel$lgb_basicos,
+  PARAM$finalmodel$optim)
+
+modelo <- lgb.train(
+  data = dtrain,
+  param = param_completo,
+)
+
+#--------------------------------------
+# ahora imprimo la importancia de variables
+tb_importancia <- as.data.table(lgb.importance(modelo))
+archivo_importancia <- "impo.txt"
+
+fwrite(tb_importancia,
+  file = archivo_importancia,
+  sep = "\t"
+)
+
+#--------------------------------------
+
+
+# aplico el modelo a los datos sin clase
+dapply <- dataset[foto_mes == PARAM$input$future]
+
+# aplico el modelo a los datos nuevos
+prediccion <- predict(
+  modelo,
+  data.matrix(dapply[, campos_buenos, with = FALSE])
+)
+
+# genero la tabla de entrega
+tb_entrega <- dapply[, list(numero_de_cliente, foto_mes)]
+tb_entrega[, prob := prediccion]
+
+# grabo las probabilidad del modelo
+fwrite(tb_entrega,
+  file = paste0("prediccion_",num,"_.txt"),
+  sep = "\t"
+)
+
+# ordeno por probabilidad descendente
+setorder(tb_entrega, -prob)
+
+
+# genero archivos con los  "envios" mejores
+# deben subirse "inteligentemente" a Kaggle para no malgastar submits
+# si la palabra inteligentemente no le significa nada aun
+# suba TODOS los archivos a Kaggle
+# espera a la siguiente clase sincronica en donde el tema sera explicado
+
+cortes <- seq(8000, 15000, by = 500)
+for (envios in cortes) {
+  tb_entrega[, Predicted := 0L]
+  tb_entrega[1:envios, Predicted := 1L]
+
+  fwrite(tb_entrega[, list(numero_de_cliente, Predicted)],
+    file = paste0(PARAM$experimento, "_",num,"_", envios, ".csv"),
+    sep = ","
+  )
 }
-cat("\n\nTAMOOO\n")
-
+}
+cat("\n\nLa generacion de los archivos para Kaggle ha terminado\n")
