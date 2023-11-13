ffa03170c3a6a9935ea2692ff3ead9cfe4fa3468
mbringas <maubringas@gmail.com>
2023-11-13 10:33:08 +0000

    ekdsamlk

--- experimentos_colaborativos/analysis/get_actual_vals.r
+++ experimentos_colaborativos/analysis/get_actual_vals.r
@@ -11,24 +11,14 @@ require("data.table")
 require("rlist")
 
 
-dataset <- "/home/maubringas/buckets/b1/datasets/colaborativos_features.csv.gz"
+dataset <- "/home/maubringas/buckets/b1/datasets/colaborativos_withTargets.csv.gz"
 
-# los meses en los que vamos a entrenar
-#  mucha magia emerger de esta eleccion
-testing <- c(202107)
-
-# cargo el dataset donde voy a entrenar el modelo
 dataset <- fread(dataset)
 
-# agrego lag1, lag3 y lag6
-all_columns <-  c("numero_de_cliente", "clase_ternaria")
-
-# defino los datos de testing
 dataset[ foto_mes == 202107,clase01 := ifelse(clase_ternaria == "BAJA+2", 1L, 0L)]
 
-dataset[,.(numero_de_cliente,clase01)
+dataset=dataset[,.(numero_de_cliente,clase01)]
 fwrite(dataset,"data_test.csv")
 # libero espacio
 gc()
 
-cat("\n\nLa optimizacion Bayesiana ha terminado\n")
